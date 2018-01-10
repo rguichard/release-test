0a82c4fec5cb40ae03e07580a5e7ee1e760fdb30
rguichard <rguichard@vsense.fr>
2018-01-10 16:23:28 +0100

    update make.sh

--- .ci/make.sh
+++ .ci/make.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 WORKDIR=$(pwd)
-echo "gh-torrrent: $(ls gh-torrent)"
+echo "gh-torrrent: $(ls -Rla gh-torrent)"
 echo "tag: $(cat gh-release-torrent/tag)"
+
+export DOCKER_VERSION=$(cat gh-release-torrent/tag)
+
+bash templates/torrent.template
+
+head templates/torrent.yml
+
+cp templates/torrent.yml output/
